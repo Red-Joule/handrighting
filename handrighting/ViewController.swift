b9ab8884292011c1975a6b109d080c487d35e6d7
Eric Mooney <eric.c.mooney@gmail.com>
2016-10-03 19:50:13 -0400

    Initial Commit

---
+++ handrighting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  handrighting
+//
+//  Created by Eric Mooney on 10/3/16.
+//  Copyright © 2016 Red Joule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
