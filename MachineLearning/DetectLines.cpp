0071235698080a7e2a97adc4950e578b20e2766b
Luke Osborne <losborne@bu.edu>
2016-10-26 19:09:36 -0400

    resize image so we can see it all

--- MachineLearning/DetectLines.cpp
+++ MachineLearning/DetectLines.cpp
@@ -14,38 +14,46 @@ int main(int argc, char* argv[])
 {
     Mat large = imread(INPUT_FILE);
     Mat rgb;
+
     // downsample and use it for processing
     pyrDown(large, rgb);
     Mat small;
     cvtColor(rgb, small, CV_BGR2GRAY);
+
     // morphological gradient
     Mat grad;
     Mat morphKernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
     morphologyEx(small, grad, MORPH_GRADIENT, morphKernel);
+
     // binarize
     Mat bw;
     threshold(grad, bw, 0.0, 255.0, THRESH_BINARY | THRESH_OTSU);
+
     // connect horizontally oriented regions
     Mat connected;
     morphKernel = getStructuringElement(MORPH_RECT, Size(150, 1));
     morphologyEx(bw, connected, MORPH_CLOSE, morphKernel);
+
     // find contours
     Mat mask = Mat::zeros(bw.size(), CV_8UC1);
     vector<vector<Point>> contours;
     vector<Vec4i> hierarchy;
     findContours(connected, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
     // filter contours
     for(int idx = 0; idx >= 0; idx = hierarchy[idx][0])
     {
         Rect rect = boundingRect(contours[idx]);
         Mat maskROI(mask, rect);
         maskROI = Scalar(0, 0, 0);
+
         // fill the contour
         drawContours(mask, contours, idx, Scalar(255, 255, 255), CV_FILLED);
+
         // ratio of non-zero pixels in the filled region
         double r = (double)countNonZero(maskROI)/(rect.width*rect.height);
 
-        if (r > .45 /* assume at least 45% of the area is filled if it contains text */
+        if (r > .25 /* assume at least 45% of the area is filled if it contains text */
             && 
             (rect.height > 8 && rect.width > 8) /* constraints on region size */
             /* these two conditions alone are not very robust. better to use something 
@@ -56,6 +64,11 @@ int main(int argc, char* argv[])
         }
     }
     imwrite(OUTPUT_FOLDER_PATH + string("rgb.jpg"), rgb);
+	cv::Mat imgout = cv::imread(INPUT_FILE);
+	resize(imgout, imgout, Size(imgout.cols/4,imgout.rows/4));
+	namedWindow( "Display Frame",CV_WINDOW_AUTOSIZE);
+	cv::imshow("output", imgout);
+	int intChar = cv::waitKey(0);           // get key press
 
     return 0;
 }
